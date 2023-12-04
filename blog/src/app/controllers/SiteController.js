59e60b94e9605ed09ae12d91789d98c93060a9ff
Duc Di Code Dao <ntduc2810@gmail.com>
2023-12-04 16:07:12 +0700

    update

--- blog/src/app/controllers/SiteController.js
+++ blog/src/app/controllers/SiteController.js
@@ -1,13 +1,22 @@
-class SiteController {
-    // [GET]/new
-    index(req, res) {
-        res.render('home');
-    }
+const Course = require("../models/Course");
 
-    // [GET]/new/:slug
-    search(req, res) {
-        res.render('search');
-    }
+class SiteController {
+  // [GET] /news
+  index(req, res, next) {
+    Course.find({})
+      .lean()
+      .then((courses) => {
+        res.render("home", { courses });
+      })
+      .catch((error) => {
+        next(error);
+      });
+  }
+
+  // [GET] /search
+  search(req, res) {
+    res.render("search");
+  }
 }
 
 module.exports = new SiteController();
---
+++ blog/src/app/models/Course.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+var slug = require('mongoose-slug-generator');
+
+mongoose.plugin(slug);
+
+const Schema = mongoose.Schema;
+
+const Course = new Schema({
+  name: { type: String, required: true },
+  description: { type: String },
+  image: { type: String },
+  slug: { type: String, slug: 'name', unique: true },
+  videoId: { type: String, required: true },
+  level: { type: String },
+}, {
+  timestamps: true,
+});
+
+module.exports = mongoose.model('Course', Course);
